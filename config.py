c2bb26b416aec74f3158575b056890682fb8b4f0
Conor Dewey <cdewey96@vt.edu>
2017-08-28 12:16:23 -0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-#Twitter Credentials
-access_token = "752258740435333121-dLXk2gShUYq9SzfVZHUIPuBnaDG91hY"
-access_token_secret = "vdrOFfoRcvSgqY6I4ni9c5quP4EqW2Jj3Ga2nLG2tz5QE"
-consumer_key = "Zc9wagvFpuUd1ppq3qq31nJJ2"
-consumer_secret = "7GIBGTeXzFtd1eCvwUnwBmNWyOPr7NpTkcxENrKNWePRYpfbd9"
